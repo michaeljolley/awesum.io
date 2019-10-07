67b04c5cc0b5960b1cb4134d581a3f5082ddf888
Michael Jolley <mike@sparcapp.io>
2019-10-06 21:23:23 -0500

    Modifying classes

--- src/core/AwesumIO.Core.Common/Classes/Tweet.cs
+++ src/core/AwesumIO.Core.Common/Classes/Tweet.cs
@@ -2,19 +2,8 @@
 
 namespace AwesumIO.Core.Common
 {
-    public class Tweet
+    public class Tweet : Gramercy
     {
-        public string TweetText { get; set; }
-        public string TweetId { get; set; }
-        public string CreatedAt { get; set; }
-        public DateTime CreatedAtISO { get; set; }
-        public int RetweetCount { get; set; }
-        public string TweetedBy { get; set; }
-        public string TweetLanguageCode { get; set; }
-        public string TweetInReplyToUserId { get; set; }
-        public bool Favorited { get; set; }
         public UserMention[] UserMentions { get; set; }
-        public object OriginalTweet { get; set; }
-        public UserDetail UserDetails { get; set; }
     }
 }
